8969003c8036bda6defef24e294e939401618bb4
Tyler Thompson <tyler.thompson@wwt.com>
2018-08-18 15:13:50 -0600

    Tweak to point to the fork - TT

--- deploy_pod.sh
+++ deploy_pod.sh
@@ -13,5 +13,5 @@ git tag "`podspec-bump --dump-version`"
 git push origin HEAD -u $(podspec-bump --dump-version)
 git reset --hard
 git clean -df
-curl --data "{\"tag_name\": \"`podspec-bump --dump-version`\",\"target_commitish\": \"master\",\"name\": \"`podspec-bump --dump-version`\",\"body\": \"Release of version `podspec-bump --dump-version`\",\"draft\": false,\"prerelease\": false}" "https://api.github.com/repos/nallick/UIUTest/releases?access_token=$PERSONAL_ACCESS_TOKEN"
+curl --data "{\"tag_name\": \"`podspec-bump --dump-version`\",\"target_commitish\": \"master\",\"name\": \"`podspec-bump --dump-version`\",\"body\": \"Release of version `podspec-bump --dump-version`\",\"draft\": false,\"prerelease\": false}" "https://api.github.com/repos/Tyler-Keith-Thompson/UIUTest/releases?access_token=$PERSONAL_ACCESS_TOKEN"
 pod trunk push UIUTest.podspec
