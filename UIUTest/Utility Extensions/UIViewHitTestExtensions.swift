741806eef87dd86c2dfe4f52080789f8ee743126
Nick Nallick <nick.nallick@asynchrony.com>
2018-10-07 12:02:48 -0600

    Add UIViewHitTestExtensions to help debug hit testing

---
+++ UIUTest/Utility Extensions/UIViewHitTestExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewHitTestExtensions.swift
+//  UIUTestExample
+//
+//  Created by Nick Nallick on 10/7/18.
+//  Copyright © 2018 Purgatory Design. All rights reserved.
+//
+
+import Foundation
