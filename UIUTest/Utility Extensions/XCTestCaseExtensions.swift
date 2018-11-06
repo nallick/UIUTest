32a06678002496b914b6b4f53c7bc4a5192bdce1
Nick Nallick <nick.nallick@asynchrony.com>
2018-11-06 14:59:10 -0700

    Add XCTestCase.flushDispatchQueue utility

---
+++ UIUTest/Utility Extensions/XCTestCaseExtensions.swift
@@ -0,0 +1,24 @@
+//
+//  XCTestCaseExtensions.swift
+//
+//  Copyright © 2018 Purgatory Design. Licensed under the MIT License.
+//
+
+import XCTest
+
+public extension XCTestCase
+{
+	/// Wait for all closures on a DispatchQueue to be dispatched.
+	///
+	/// - Parameters:
+	///   - dispatchQueue: the queue to wait for
+	///   - seconds: the wait timeout
+	///   - description: the expectation description
+	///
+	public func flushDispatchQueue(_ dispatchQueue: DispatchQueue = DispatchQueue.main, timeout seconds: TimeInterval = 0.1, description: String = "Wait for DispatchQueue") {
+		let expectation = self.expectation(description: description)
+		dispatchQueue.async { expectation.fulfill() }
+		self.wait()
+		self.wait(for: [expectation], timeout: timeout)
+	}
+}
