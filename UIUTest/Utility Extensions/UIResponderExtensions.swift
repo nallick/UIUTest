f156b05d9cf832d4acd8f84ff4f6075b0d018d5f
Nick Nallick <nick.nallick@asynchrony.com>
2019-03-24 14:30:21 -0600

    Rename first responder utilities to next responder to avoid confusion

--- UIUTest/Utility Extensions/UIResponderExtensions.swift
+++ UIUTest/Utility Extensions/UIResponderExtensions.swift
@@ -13,8 +13,8 @@ public extension UIResponder
 	/// - Parameter type: The type of the responder to find.
 	/// - Returns: The first responder of the specified type (if any).
 	///
-	func firstResponder<T>(ofType type: T.Type) -> T? {
-		return self.next as? T ?? self.next.flatMap { $0.firstResponder(ofType: type) }
+	func nextResponder<T>(ofType type: T.Type) -> T? {
+		return self.next as? T ?? self.next.flatMap { $0.nextResponder(ofType: type) }
 	}
 
 	/// Returns the next responder in the receiver's responder chain which matches the supplied predicate.
@@ -22,8 +22,8 @@ public extension UIResponder
 	/// - Parameter predicate: The function to specify matches.
 	/// - Returns: The first responder which matches the predicate (if any).
 	///
-	func firstResponder(where predicate: @escaping (UIResponder) -> Bool) -> UIResponder? {
+	func nextResponder(where predicate: @escaping (UIResponder) -> Bool) -> UIResponder? {
 		guard let nextResponder = self.next else { return nil }
-		return predicate(nextResponder) ? nextResponder : self.next.flatMap { $0.firstResponder(where: predicate) }
+		return predicate(nextResponder) ? nextResponder : self.next.flatMap { $0.nextResponder(where: predicate) }
 	}
 }
